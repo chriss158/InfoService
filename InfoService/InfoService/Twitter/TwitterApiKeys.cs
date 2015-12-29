0b2c84522b391d0aa6f394282546e51942e2c684
hasenbolle <edgi@arcor.de>
2015-12-29 16:39:45 +0100

    Again small refactoring

--- InfoService/InfoService/Twitter/TwitterApiKeys.cs
+++ InfoService/InfoService/Twitter/TwitterApiKeys.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace InfoService.Twitter
+﻿namespace InfoService.Twitter
 {
     public static class TwitterApiKeys
     {
