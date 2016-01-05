157140e68837c59ad14b213ad8552145d31f54b3
hasenbolle <edgi@arcor.de>
2016-01-05 18:51:09 +0100

    Fixed problems of two many new feeds messages on some feeds

--- InfoService/InfoService/Feeds/FeedReader/FeedItemComparer.cs
+++ InfoService/InfoService/Feeds/FeedReader/FeedItemComparer.cs
@@ -8,16 +8,21 @@ namespace FeedReader
     {
         public bool Equals(FeedItem x, FeedItem y)
         {
-            return x.Title == y.Title;
+            //return x.Title == y.Title || x.Url == y.Url;
+            if (x.Title == y.Title) return true;
+            if (x.Url == y.Url) return true;
+            return false;
         }
 
         public int GetHashCode(FeedItem obj)
         {
             if (Object.ReferenceEquals(obj, null)) return 0;
 
-            int hashTitle = obj.Title == null ? 0 : obj.Title.GetHashCode();
+            int hashTitle = 13;
+            //hashTitle = (hashTitle * 7) + obj.Title.GetHashCode();
+            hashTitle = (hashTitle * 7) + obj.Url.GetHashCode();
 
             return hashTitle;
-        }  
+        }
     }
 }
