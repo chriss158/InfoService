ec028a43a859e1b0e9ea715bd1a975c54cae9653
hasenbolle <edgi@arcor.de>
2016-01-15 14:47:22 +0100

    Update LoadParameterParser.cs

--- InfoService/InfoService/Utils/LoadParameterParser.cs
+++ InfoService/InfoService/Utils/LoadParameterParser.cs
@@ -8,63 +8,48 @@ namespace InfoService.Utils
 {
     public class LoadParameterParser
     {
-
         string _loadParameters;
-        private List<LoadParameter> _parameters;
+        private LoadParameters _parameters;
+
+        const string SearchRegexString = "(\\w*):(\".*?\")";
+        //const string SearchRegexString = @"(\w*):[^,\\]*(?:\\.[^,\\]*)*";
 
         public LoadParameterParser(string loadParameters)
         {
             _loadParameters = loadParameters;
-            //_parameters = new List<LoadParameter<T>>();
+            _parameters = new LoadParameters();
             ParseParameters();
         }
 
         public LoadParameter GetParameter(string parameterName)
         {
-            return _parameters.FirstOrDefault(parameter => parameter.ParameterName == parameterName);
+            if (_parameters.Contains(parameterName))
+            {
+                return _parameters.FirstOrDefault(parameter => parameter.ParameterName == parameterName);
+            }
+            else return new LoadParameter();
         }
 
         private void ParseParameters()
         {
             if (!string.IsNullOrEmpty(_loadParameters))
             {
-                Regex regex = new Regex(@"(\w*):[^,\\]*(?:\\.[^,\\]*)*");
+                Regex regex = new Regex(SearchRegexString);
                 MatchCollection coll = regex.Matches(_loadParameters);
                 foreach (Match match in coll)
                 {
                     string[] param = match.Value.Split(':');
                     if (param.Length == 2)
                     {
-                        LoadParameter loadParam = null;
                         string paramName = param[0].Trim();
                         string paramSetting = param[1].Trim();
                         if (paramName.Length > 0 && paramSetting.Length > 0)
                         {
-                            if (paramSetting.All(char.IsDigit))
-                            {
-                                try
-                                {
-                                    loadParam = new LoadParameter<int>(paramName, Convert.ToInt32(paramSetting));
-                                }
-                                catch (Exception)
-                                {
-                                    loadParam = null;
-                                }
-                            }
-                            else
-                            {
-                                loadParam = new LoadParameter<string>(paramName, paramSetting);
-                            }
-                        }
-                        if (loadParam != null)
-                        {
-                            _parameters.Add(loadParam);
+                            _parameters.Add(new LoadParameter(paramName, paramSetting));
                         }
                     }
                 }
             }
         }
-
     }
-
 }
